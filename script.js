d93fa5ef1dedd36b82cfc44ae1986b031cfd8aec
MattiaPARRINELLO <contactperso.mp@gmail.com>
2022-04-19 21:53:37 +0200

    Created a way to downalod prods by code

---
+++ script.js
@@ -0,0 +1,14 @@
+function download() {
+    var input = document.getElementById("code");
+    var json = $.getJSON('/codes.json').done(function () {
+        var codes = json.responseJSON
+        console.log(codes)
+        console.log(codes.beta + codes.title)
+        for (var i = 0; i < codes.beta.length; i++) {
+            var a = codes.beta[i]
+            if (a.includes(input.value)) {
+                window.location.href = "/Prods/" + codes.title[i++] + ".mp3"
+            }
+        }
+    });
+}
